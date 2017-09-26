3149b2e2b912acca38b0f4f47fdd3e4912631608
kitayuguchiryuhei <kitayuguchiryuhei@MacBook-Pro.local>
2017-09-26 09:07:16 +0900

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,9 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var compass = require("gulp-compass");
 var autoprefixer = require("gulp-autoprefixer");
 var frontnote = require("gulp-frontnote");
 var uglify = require("gulp-uglify");
 var plumber = require("gulp-plumber");
-//var browser = require("browser-sync");
-
-/*gulp.task("server", function(){
-    browser({
-        server: {
-            baseDir: "./"
-        }
-    });
-});*/
 
 gulp.task("js", function(){
 	gulp.src(["js/**/*.js","!js/min/**/*.js"])
@@ -22,19 +13,21 @@ gulp.task("js", function(){
         .pipe(browser.reload({stream:true}));
 });
 
-gulp.task("sass", function(){
-	gulp.src("sass/**/*.scss")
+gulp.task("compass", function(){
+    gulp.src("sass/**/*.scss")
 		.pipe(plumber())
 		.pipe(frontnote({
 			css: '../css/design.css'
 		}))
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest("./css"))
-        .pipe(browser.reload({stream:true}));
+        .pipe(compass({
+            config_file: 'config.rb',
+            sass: 'sass',
+            css: 'css'
+        }));
 });
 
 gulp.task("default",/* ['server'],*/ function(){
 	gulp.watch(	["js/**/*.js","!js/min/**/.js"], ["js"]);
-	gulp.watch("sass/**/*.scss", ["sass"]);
+	//gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("sass/**/*.scss", ["compass"]);
 });
