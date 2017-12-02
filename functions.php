601e1706b644dd25c08b5f835df8a682c02f9d38
kitayuguchiryuhei <kitayuguchiryuhei@MacBook-Pro.local>
2017-12-03 00:05:54 +0900

    背景画像　設定できるようにfunctions.php変更

--- functions.php
+++ functions.php
@@ -10,3 +10,9 @@ register_sidebar(array(
     'name' => 'サイドバー２',
     'id' => 'sidebar2'
 ));
+
+$custom_background_defaults = array(
+        'default-color' => 'efefef',
+        'default-image' => get_bloginfo('template_url') . '/images/bg_wall.gif',
+);
+add_theme_support( 'custom-background', $custom_background_defaults );
